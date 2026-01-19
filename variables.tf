9fcf8954f4a822b8eb286d6fd77849b25f7d301b
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-19 15:51:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,55 +1,48 @@
-
-
-# -----------------------------
-# Git Repo & Branch
-# -----------------------------
 variable "git_repo_url" {
-  description = "https://github.com/OpsMx/enterprise-ssd.git"
+  description = "URL of the Git repository containing the OpsMx SSD Helm chart"
   type        = string
+  default     = "https://github.com/OpsMx/enterprise-ssd.git"
 }
 
 variable "git_branch" {
-  description = "Git branch to clone (for upgrades)"
+  description = "Git branch to clone (can be updated for upgrades)"
   type        = string
-  default     = "2025-07"
+  default     = "2025-01"
 }
 
-# -----------------------------
-# Kubernetes Config
-# -----------------------------
 variable "kubeconfig_path" {
-  description = "Path to kubeconfig file"
+  description = "Path to your kubeconfig file"
   type        = string
   default     = ""  # Empty means use in-cluster config
+
 }
 
+
+# Ingress Configuration
+# ---------------------------------------------
+variable "ingress_hosts" {
+  description = "The DNS hostname for the SSD UI (must be lowercase)"
+  type        = list(string)
+}
+
+
 variable "namespace" {
-  description = "Namespace to deploy OpsMx SSD"
+  description = "Kubernetes namespace to deploy SSD"
   type        = string
   default     = "ssd-tf"
 }
 
-# -----------------------------
-# Ingress Hosts
-# -----------------------------
-variable "ingress_hosts" {
-  description = "List of DNS hostnames for SSD UI"
-  type        = list(string)
-  
-}
 
-# -----------------------------
-# Helm Release & Cert Manager
-# -----------------------------
 variable "helm_release_name" {
-  description = "Name of Helm release"
+  description = "Name of the Helm release"
   type        = string
   default     = "ssd-opsmx-terraform"
 }
+
+
 
 variable "cert_manager_installed" {
   description = "Set to true if cert-manager is installed"
   type        = bool
   default     = true
 }
-
