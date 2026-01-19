d194bbc5837b02b633de23039a5a0a56c91382ce
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-19 19:29:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
-output "helm_release_names" {
-  value = { for host, r in helm_release.opsmx_ssd : host => r.name }
+output "helm_release_name" {
+  description = "SSD Helm release name"
+  value       = helm_release.opsmx_ssd.name
 }
 
-output "helm_namespaces" {
-  value = { for host, r in helm_release.opsmx_ssd : host => r.namespace }
+output "helm_release_namespace" {
+  description = "Namespace where SSD is deployed"
+  value       = helm_release.opsmx_ssd.namespace
+}
+
+output "ssd_version" {
+  description = "SSD Helm chart version deployed"
+  value       = helm_release.opsmx_ssd.version
 }
